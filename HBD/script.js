dbee6f088fbe00771185c4a1121f0eb9cd8fe528
Emmanuelle T Villaranda <emmanuelletvillaranda@Emmanuelles-MacBook-Air.local>
2026-08-07 11:22:28 +0800

    Add

--- HBD/script.js
+++ HBD/script.js
@@ -268,7 +268,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let trailParticles = [];
     let heartSparks = [];
     const imageSources = [
-        "https://raw.githubusercontent.com/Emman839/Coding/15d2ebebfd79564d1145f33c5cd58bd2bf7b0858/HBD/src/babii.png?token=GHSAT0AAAAAAEE5Y66JGTGMO2KUH3M7F4H42TVJAFQ",,
+        "https://raw.githubusercontent.com/Emman839/Coding/refs/heads/main/HBD/src/babii.png",
         "https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2?w=150&auto=format&fit=crop&q=80",
         "https://images.unsplash.com/photo-1513272795190-0b7c527757ed?w=150&auto=format&fit=crop&q=80"
     ];
